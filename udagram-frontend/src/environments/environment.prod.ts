a809af60cac68b309502d6019473b833f3071955
omarNawito <o_mohsen@hotmail.com>
2022-03-12 23:38:54 +0200

    change api host

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "demo-app",
+  apiHost: "http://demoapp-env.eba-wamqrjcn.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
